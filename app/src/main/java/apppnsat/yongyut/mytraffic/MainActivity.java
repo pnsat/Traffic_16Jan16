f77b1cedae44d936d793843f99e63f54b13d0dae
masterUNF <ubc1515@hotmail.com>
2016-01-16 14:31:28 +0700

    Bind Widget Finish

--- app/src/main/java/apppnsat/yongyut/mytraffic/MainActivity.java
+++ app/src/main/java/apppnsat/yongyut/mytraffic/MainActivity.java
@@ -2,12 +2,38 @@ package apppnsat.yongyut.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private ListView TrafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Bind Widget
+        bindWidget();
+
+
+
+
+    }  // Main Method
+
+    private void bindWidget() {
+        TrafficListView = (ListView) findViewById(R.id.livTraffic);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+
+
+
+
     }
-}
+} // Main Class
