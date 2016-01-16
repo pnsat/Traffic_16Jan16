67215f9598445e54066fccd17436f2bdc70b7f64
masterUNF <ubc1515@hotmail.com>
2016-01-16 14:58:41 +0700

    button

--- app/src/main/java/apppnsat/yongyut/mytraffic/MainActivity.java
+++ app/src/main/java/apppnsat/yongyut/mytraffic/MainActivity.java
@@ -1,7 +1,9 @@
 package apppnsat.yongyut.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -22,17 +24,33 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
 
-
-
+        buttonController();
     }  // Main Method
+
+    private void buttonController() {
+
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_shut);
+                buttonMediaPlayer.start();
+
+            }  //event
+        });
+
+
+
+    }
 
     private void bindWidget() {
         TrafficListView = (ListView) findViewById(R.id.livTraffic);
         aboutMeButton = (Button) findViewById(R.id.button);
-
-
-
 
 
     }
