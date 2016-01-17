ebc5c2b09b515557207d9c963b4aa3e845b0a7d2
masterUNF <ubc1515@hotmail.com>
2016-01-17 12:13:29 +0700

    GOOD

--- app/src/main/java/apppnsat/yongyut/mytraffic/MainActivity.java
+++ app/src/main/java/apppnsat/yongyut/mytraffic/MainActivity.java
@@ -29,7 +29,59 @@ public class MainActivity extends AppCompatActivity {
         //Button Controller
 
         buttonController();
+
+        //ListView Controller
+        ListViewController();
+
+
+
+
     }  // Main Method
+
+    private void ListViewController() {
+
+        //1#
+        int[] iconInts = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
+                R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
+                R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
+                R.drawable.traffic_12, R.drawable.traffic_13, R.drawable.traffic_14,
+                R.drawable.traffic_15, R.drawable.traffic_16, R.drawable.traffic_17,
+                R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
+
+        //#2
+        String[] titleStrings = new String[20];
+        titleStrings[0] = "หัวขัอหลัก ที่ 1";
+        titleStrings[1] = "หัวขัอหลัก ที่ 2";
+        titleStrings[2] = "หัวขัอหลัก ที่ 3";
+        titleStrings[3] = "หัวขัอหลัก ที่ 4";
+        titleStrings[4] = "หัวขัอหลัก ที่ 5";
+        titleStrings[5] = "หัวขัอหลัก ที่ 6";
+        titleStrings[6] = "หัวขัอหลัก ที่ 7";
+        titleStrings[7] = "หัวขัอหลัก ที่ 8";
+        titleStrings[8] = "หัวขัอหลัก ที่ 9";
+        titleStrings[9] = "หัวขัอหลัก ที่ 10";
+        titleStrings[10] = "หัวขัอหลัก ที่ 11";
+        titleStrings[11] = "หัวขัอหลัก ที่ 12";
+        titleStrings[12] = "หัวขัอหลัก ที่ 13";
+        titleStrings[13] = "หัวขัอหลัก ที่ 14";
+        titleStrings[14] = "หัวขัอหลัก ที่ 15";
+        titleStrings[15] = "หัวขัอหลัก ที่ 16";
+        titleStrings[16] = "หัวขัอหลัก ที่ 17";
+        titleStrings[17] = "หัวขัอหลัก ที่ 18";
+        titleStrings[18] = "หัวขัอหลัก ที่ 19";
+        titleStrings[19] = "หัวขัอหลัก ที่ 20";
+
+        //#3
+        String[] descriptionStrings = getResources().getStringArray(R.array.description);
+
+        //Create ListView
+        MyAdapter objMyAdapter = new MyAdapter(getBaseContext(), iconInts,
+                titleStrings, descriptionStrings);
+        TrafficListView.setAdapter(objMyAdapter);
+
+    }
+
 
     private void buttonController() {
 
@@ -50,12 +102,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+
+
   }  //event
         });
 
 
     }
-
     private void bindWidget() {
         TrafficListView = (ListView) findViewById(R.id.livTraffic);
         aboutMeButton = (Button) findViewById(R.id.button);
